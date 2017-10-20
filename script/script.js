40a56bdfdf0fcdcf052ece23510880dd864424e9
carlos <carlosvizcaino718@gmail.com>
2017-10-20 10:54:33 -0400

    setting up repo

---
+++ script/script.js
@@ -0,0 +1,11 @@
+
+
+// here i'm going to create a game
+
+var score, roundscore, activeplayer, dice;
+score=[0,0];
+roundscore=0;
+activeplayer=0;
+
+dice= math.floor(math.random()*6)+1;
+console.log(dice);
